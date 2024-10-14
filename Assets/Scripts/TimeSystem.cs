1dbefe7ebcda2bc86af0f57faf31ed4eeb0d3b06
Shaurya Magar <64362792+ShauryaMagar@users.noreply.github.com>
2024-10-13 20:00:50 -0700

    Update TimeSystem.cs

--- Assets/Scripts/TimeSystem.cs
+++ Assets/Scripts/TimeSystem.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement; // Added for scene management
 
 public class TimeSystem : MonoBehaviour
